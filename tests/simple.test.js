77a01ac861e5ce65e66e77f612e13085bc8d833f
Uladzislau Yerashevich <74885175+vladyera@users.noreply.github.com>
2024-07-30 23:28:05 -0700

    4.5

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -44,4 +44,53 @@ describe('total likes', () => {
     const totalLikes = newList.reduce((sum, blog) => sum + blog.likes, 0);
     assert.strictEqual(totalLikes, totalLikesResult);
   });
+});
+
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ];
+
+  const listWithMultipleBlogs = [
+    ...listWithOneBlog,
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Some Title',
+      author: 'Vlad',
+      url: 'https://google.com',
+      likes: 15,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17fa',
+      title: 'Another Blog',
+      author: 'Alice',
+      url: 'https://example.com',
+      likes: 25,
+      __v: 0
+    }
+  ];
+
+  test('of empty list is null', () => {
+    const emptyList = [];
+    const favorite = listHelper.favoriteBlog(emptyList);
+    assert.strictEqual(favorite, null);
+  });
+
+  test('when list has only one blog, equals that blog', () => {
+    const favorite = listHelper.favoriteBlog(listWithOneBlog);
+    assert.deepStrictEqual(favorite, listWithOneBlog[0]);
+  });
+
+  test('of a bigger list is calculated right', () => {
+    const favorite = listHelper.favoriteBlog(listWithMultipleBlogs);
+    assert.deepStrictEqual(favorite, listWithMultipleBlogs[2]);
+  });
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,6 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  return blogs.reduce((favorite, blog) => {
+    return (blog.likes > favorite.likes) ? blog : favorite;
+  });
+};
+
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 };
